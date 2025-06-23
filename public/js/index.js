3a802546d8fa92a2d9cd39032f82d5b7169300cc
Christopher Kaelin <chriskaelin67@gmail.com>
2025-06-23 07:10:58 -0400

    Updated to hide key and get the random image from API

--- public/js/index.js
+++ public/js/index.js
@@ -6,17 +6,17 @@
 // };
 
 // const quotes = [
-//     {quote: "Be curious, not judgemental",
-//      author: "Ted Lasso",
-//     },
-//     {quote: "I hope that all of us or none of us are judged by the actions of our weakest moments; but rather by the strength we show when and if we are ever given a second chance",
-//      author: "Ted Lasso",
-//     },
 //     {quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
 //      author: "Winston S. Churchill",
 //     },
-//     {quote: "It's not what we do once in a while that shapes our lives, but what we do consistently.",
-//      author: "Tony Robbins",
+//     {quote: "Be curious, not judgemental",
+//      author: "Ted Lasso",
+//     },
+//     {quote: "When you die, it does not mean that you lose to cancer. You beat cancer by how you live, why you live, and in the manner in which you live",
+//      author: "Stuart Scott",
+//     },
+//     {quote: "Every day is a chance to be better than we were the day before.",
+//      author: "Ted Lasso",
 //     },
 //     {quote: "To me there are three things everyone should do every day. Number one is laugh. Number two is think -- spend some time time in thought. Number three, you should have your emotions move you to tears. If you laugh, think and cry, that's a heck of a day.",
 //      author: "Jim Valvano",
@@ -33,19 +33,19 @@
 //         } else {
 //             quoteIndex = 0;
 //         }
-//     }, 3000);
+//     }, 30000);
 // }
 
 // setTimeout(loopThroughQuotes, 3000);
 
 
 async function getRandomImage() {
-    const client_id = "t4tYoCKn-qs37VWsASGIIT78CM6SHoRvvdMkyHOIxjI";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
+
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoURL = returnedData.urls.regular;
+        const receivedPhotoURL = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
         imgDiv.style.backgroundImage = `url("${receivedPhotoURL}")`;
@@ -55,9 +55,8 @@ async function getRandomImage() {
     }
 }
 
-getRandomImage()
 
 setInterval(() => {
     getRandomImage();
-}, 3000);
+}, 30000);
 
--- server.js
+++ server.js
@@ -5,7 +5,35 @@ const app = express();
 
 const port = 8080;
 
+require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
+
 app.use(express.static("./public"));
+
+
+//  API endpoint to get random image
+async function getRandomImage() {
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        return receivedPhotoUrl;
+        // return 'Hello, this is a placeholder for the image URL: ';
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.use("/api/v1/getRandomImage", async (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: await getRandomImage(),
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
